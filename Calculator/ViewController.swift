5a3f403a40f66ab7a406f6efb21e0235b4dd4c46
이치훈 <dlclgns152615@gmail.com>
2023-03-07 14:31:18 +0900

    Test

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK : -Properties
+    
+    var resultTextField: UITextField!
+    var buttonsContainer: UIView!
+    var firstStackView: UIStackView!
+    var secondStackView: UIStackView!
+    var thirdStackView: UIStackView!
+    var fourthStackView: UIStackView!
+    
+
+    //MARK: -LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        self.makeTextField()
+        print("test")
     }
 
 
 }
 
+extension ViewController {
+    
+    //MARK: -ObjectSetting
+    
+    fileprivate func makeTextField() {
+        resultTextField = UITextField()
+        resultTextField.text = "Result Text"
+        resultTextField.backgroundColor = .systemGray
+        self.view.addSubview(resultTextField)
+    }
+    
+    fileprivate func makeButtonContainer() {
+        buttonsContainer = UIView()
+        buttonsContainer.backgroundColor = .green
+        self.view.addSubview(buttonsContainer)
+        
+        
+        
+    }
+    
+    func stackSetting(stack: UIStackView) {
+        stack.axis = .horizontal
+        stack.spacing = 10
+        
+    }
+}
